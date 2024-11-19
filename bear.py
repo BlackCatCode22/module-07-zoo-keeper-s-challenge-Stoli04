c3b96edb51a50a1c93eae25c82242ed39a94e65e
Stoli04 <kakoa14@aol.com>
2024-11-18 21:49:45 -0800

    Update bear.py

--- bear.py
+++ bear.py
@@ -1,15 +1,15 @@
-#Import the Animal class from the Animal module
+# Import the Animal class from the Animal module
 from Animal import Animal
 
 class Bear(Animal):
-    # create a static class variable to keep track of the number of bears created
+    # create a static class variable to keep track of the number of Bears created
     numOfBears = 0
 
-    # Create the bear sound
-    bear_sound = " growl...growl "
+    # Create the Hyena sound
+    hyena_sound = " roar...roar "
 
-    # Create a list of bear names.
-    list_of_bear_names = []
+    # Create a list of hyena names.
+    list_of_hyena_names = []
 
     file_path = r'animalNames.txt'
     with open(file_path, 'r') as file:
@@ -18,31 +18,30 @@ class Bear(Animal):
         # Iterate through the lines in the file
         line_num = 1
         for line in lines:
-            if line_num == 11: # Bear names are on the 11th line
-                list_of_bear_names.extend(line.strip().split(', '))
+            if line_num == 3: # Hyenas names are on the 7th line
+                list_of_lion_names.extend(line.strip().split(', '))
                 break
             else:
                 line_num += 1
 
-    def init(self, name="a_name", animal_id="an_id", birth_date="2099-01-01", color="a_color", sex="a_sex",
-                 weight="a_weight", originating_zoo="a_zoo", date_arrival="2099-01-01"):
-        # Increment the static variable numOfBears when a new Bear object is created
-        Lion.numOfBears += 1
+    def __init__(self, name="a_name", animal_id="an_id", birth_date="2099-01-01", color="a_color", sex="a_sex", weight="a_weight", originating_zoo="a_zoo", date_arrival="2099-01-01"):
+        # Increment the static variable numOfHyenas when a new Hyena object is created
+        Hyena.numOfHyenas += 1
 
         # Generate the unique ID
         self.gen_unique_id()
 
-        # Call the constructor of the parent class (Animal) with 'Bears' as the species
-        super().init("bear", name, animal_id, birth_date, color, sex, weight, originating_zoo, date_arrival)
-
+        # Call the constructor of the parent class (Animal) with 'Lion' as the species
+        super().__init__("hyena", name, animal_id, birth_date, color, sex, weight, originating_zoo, date_arrival)
+    
     def make_sound(self):
-        return self.bear_sound
+        return self.hyena_sound
 
-    # the lion object will call this method to get an unused bear name. pop() will remove the first element from
-    #   the list_of_bear_names[]
-    def get_lion_name(self):
-        return self.list_of_bear_names.pop(0)
+    # the lion object will call this method to get an unused hyena name. pop() will remove the first element from
+    #   the list_of_hyena_names[]
+    def get_hyena_name(self):
+        return self.list_of_hyena_names.pop(0)
 
     def gen_unique_id(self):
-        # Assuming the unique ID is composed of the species abbreviated and the number of bears
-        self.animal_id = "Be" + str(Bear.numOfBears).zfill(2)
+        # Assuming the unique ID is composed of the species abbrevation and the number of hyenas
+        self.animal_id = "Hy" + str(Hyena.numOfHyenas).zfill(2)
