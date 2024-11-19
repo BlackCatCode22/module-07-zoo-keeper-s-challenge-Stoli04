e3fdd38fb1244626958d1f8754483c2eca6cb1ef
Stoli04 <kakoa14@aol.com>
2024-11-18 21:33:49 -0800

    Update lion.py

--- lion.py
+++ lion.py
@@ -1,15 +1,15 @@
 # Import the Animal class from the Animal module
 from Animal import Animal
 
-class Hyena(Animal):
-    # create a static class variable to keep track of the number of hyenas created
+class Lion(Animal):
+    # create a static class variable to keep track of the number of lions created
     numOfHyenas = 0
 
-    # Create the hyena sound
-    hyena_sound = " laugh...laugh "
+    # Create the lion sound
+    hyena_sound = " rawr...rawr "
 
-    # Create a list of hyena names.
-    list_of_hyena_names = []
+    # Create a list of lion names.
+    list_of_lion_names = []
 
     file_path = r'C:\2023spring\2023fall\python\dataFiles\animalNames.txt'
     with open(file_path, 'r') as file:
@@ -18,7 +18,7 @@ class Hyena(Animal):
         # Iterate through the lines in the file
         line_num = 1
         for line in lines:
-            if line_num == 3:
+            if line_num == 5:
                 list_of_hyena_names.extend(line.strip().split(', '))
                 break
             else:
@@ -27,15 +27,15 @@ class Hyena(Animal):
     def __init__(self, name="a_name", animal_id="an_id", birth_date="2099-01-01", color="a_color", gender="a_gender",
                  weight="a_weight", originating_zoo="a_zoo", date_arrival="2099-01-01"):
         # Increment the static variable numOfHyenas when a new Hyena object is created
-        Hyena.numOfHyenas += 1
+        Hyena.numOfLions += 1
 
         # Call the constructor of the parent class (Animal) with 'Lion' as the species
-        super().__init__("hyena", name, animal_id, birth_date, color, gender, weight, originating_zoo, date_arrival)
+        super().__init__("lion", name, animal_id, birth_date, color, gender, weight, originating_zoo, date_arrival)
 
     def make_sound(self):
-        return self.hyena_sound
+        return self.lion_sound
 
-    # the hyena object will call this method to get an unused hyena name. pop() will remove the first element from
-    #   the list_of_hyena_names[]
-    def get_hyena_name(self):
-        return self.list_of_hyena_names.pop(0)
+    # the lion object will call this method to get an unused hyena name. pop() will remove the first element from
+    #   the list_of_lion_names[]
+    def get_lion_name(self):
+        return self.list_of_lion_names.pop(0)
